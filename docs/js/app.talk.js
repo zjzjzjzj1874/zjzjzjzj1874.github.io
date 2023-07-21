54989a8c59212b55b7a00e27da0ffac1159658c7
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2023-07-21 14:15:54 +0800

    feat✨(gitalk): comment with gitalk
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ docs/js/app.talk.js
@@ -0,0 +1,11 @@
+const gitalk = new Gitalk({
+    clientID: '97308c7c36d421da15da',
+    clientSecret: '0983f03d3be4bc4c775b9cef0caafb9964befea6',
+    repo: 'zjzjzjzj-comments',
+    owner: 'zjzjzjzj1874',
+    admin: [
+        'zjzjzjzj1874',
+    ],
+    // facebook-like distraction free mode
+    distractionFreeMode: false,
+});
